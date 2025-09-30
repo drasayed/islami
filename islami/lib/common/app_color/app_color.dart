72968f1c50b787d2a9c1ec5b02c442cd165d6734
amr <dr.asayed@gmail.com>
2025-10-01 01:38:09 +0300

    app colors

---
+++ islami/lib/common/app_color/app_color.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class AppColor {
+static const Color black = Color(0xff202020) ;
+static const Color gold =Color(0xffE2BE7F);
+static const Color grey = Color(0xff707070);
+static const Color offWhite =Color(0xffFFF5E3);
+static const Color brown = Color(0xff946107);
+
+}
